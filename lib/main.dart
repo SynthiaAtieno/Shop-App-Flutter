e692f88dde5386ac2042c78a4ae925fdb388537c
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-07-16 22:58:26 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData.from(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 241, 218, 7))),
     );
   }
 }
